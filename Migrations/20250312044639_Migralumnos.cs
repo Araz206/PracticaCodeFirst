e45b349463eb6b184ddb594fb38a91d989b124da
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-03-11 22:50:02 -0600

    Creamosla migracion con la base de datos

---
+++ Migrations/20250312044639_Migralumnos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practica1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migralumnos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumnos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroDeControl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Carrera = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaDeNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumnos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alumnos");
+        }
+    }
+}
