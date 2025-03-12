aea4bfbb3dcaeb5725331c3dcb8e559e3c11aba3
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-03-11 22:38:56 -0600

    Agregamos la clases alumno

---
+++ Models/Alumno.cs
@@ -0,0 +1,12 @@
+﻿namespace Practica1.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NumeroDeControl { get; set; }
+        public string Carrera { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+    }
+}
