e8cdffd1f1ea191f60e3b398bb8f700b4076f1cc
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-03-11 22:42:58 -0600

    Agregamos el DbContext

---
+++ Models/AlumnoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica1.Models
+{
+    public class AlumnoDbContext : DbContext
+    {
+        public AlumnoDbContext(DbContextOptions<AlumnoDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+
+    }
+}
